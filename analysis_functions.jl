61c825ed2199e952018fa46bf2b4f1aa52d0c4e2
aespinosav <alonso.espinosa@bristol.ac.uk>
2016-03-07 12:26:03 +0000

    Added regime checking in the total_cost functions and edge_cost functions

--- analysis_functions.jl
+++ analysis_functions.jl
@@ -7,21 +7,33 @@ value in the demand range.
 
 TA problem has to have been solved
 """
-function total_cost(rn::RoadNetwork)
+function total_cost(rn::RoadNetwork, regime="UE")
+    if regime == "UE"
+        flows = rn.flows_ue
+    else
+        flows = rn.flows_so
+    end
+    
     m = num_edges(rn)
-    num_sols = size(rn.flows)[2]
+    num_sols = size(flows)[2]
     num_qs = length(rn.demand_range)
-    costs = [dot(rn.a,rn.flows[:,i]) + dot(rn.flows[:,i],diagm(rn.b)*rn.flows[:,i]) for i in 1:num_qs]
+    costs = [dot(rn.a,flows[:,i]) + dot(flows[:,i],diagm(rn.b)*flows[:,i]) for i in 1:num_qs]
 end
 
 """
 Returns the actual cost of each edge (cost * flow) in the same format as the
 flows (rows correspond to edges, columns to demand level)
 """
-function edge_costs(rn::RoadNetwork)
+function edge_costs(rn::RoadNetwork, regime="UE")
     m = num_edges(rn)
-    num_sols = size(rn.flows)[2]
-    num_qs = length(rn.demand_range)
     
-    costs = rn.flows .* (rn.a .+ rn.b .* rn.flows)
+    if regime=="UE"
+        flows = rn.flows_ue
+    else
+        flows = rn.flows_so
+    end
+    
+    num_sols = size(flows)[2]
+    num_qs = length(rn.demand_range)
+    costs = flows .* (rn.a .+ rn.b .* flows)
 end
