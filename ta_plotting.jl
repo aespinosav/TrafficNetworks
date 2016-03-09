fb17fd689608f905e503e3736ae9f6ebb8278276
aespinosav <alonso.espinosa@bristol.ac.uk>
2016-03-09 12:48:39 +0000

    Added function to plot normalised edge flows in ta_plotting. Added function to make normalised flow data frame. This brings up the question about wheter I should be using a DataFrames inside the RoadNetwork objects in the first place and have these functions defined elsewhere...

--- ta_plotting.jl
+++ ta_plotting.jl
@@ -5,7 +5,7 @@ Converts the flows of rn for the chosen regime (UE/SO) to a
 data frame, mostly for plotting, but could be useful for storing 
 the data. (maybe this function should be defined in another file?)
 """
-function make_data_frame_from_flows(rn::RoadNetwork, regime="UE")
+function flows_data_frame(rn::RoadNetwork, regime="UE")
     if regime=="UE"
         flows = rn.flows_ue
     else
@@ -17,8 +17,26 @@ function make_data_frame_from_flows(rn::RoadNetwork, regime="UE")
     names!(data_frame, names)
 end
 
+"""
+normalises the flows of rn with respect to total demand for the chosen regime (UE/SO) 
+and makes a data frame.
+
+removes the first row of the original flows to avoid dividing by 0.
+"""
+function normflows_data_frame(rn::RoadNetwork, regime="UE")
+    flows = flows_data_frame(rn, regime)[2:end, :]
+    for i in 2:size(flows)[2]
+        flows[i] = flows[i] ./ flows[:q]
+    end
+    flows
+end
+
+"""
+Plots the edge flows of a RoadNetwork that has been solved (with appropriate regime UE/SO).
+Uses Gadfly.
+"""
 function plot_flows(rn::RoadNetwork, regime="UE")
-    data_flows = make_data_frame_from_flows(rn, regime)
+    data_flows = flows_data_frame(rn, regime)
     #layers = [ layer( x=data_flows[1], y=data_flows[i], Geom.line, Theme(defaul_color=distinguishable_colors(size(data_flows)[2])[i])) for i in 2:length(flows) ]
     plot(melt(data_flows, :q), 
          x=:q, 
@@ -32,17 +50,20 @@ function plot_flows(rn::RoadNetwork, regime="UE")
          Guide.Title("Edge flows") )
 end
 
-# function plot_normalised_flows(rn::RoadNetwork, regime="UE")
-#     data_flows = make_data_frame_from_flows(rn, regime)
-#     #layers = [ layer( x=data_flows[1], y=data_flows[i], Geom.line, Theme(defaul_color=distinguishable_colors(size(data_flows)[2])[i])) for i in 2:length(flows) ]
-#     plot(melt(data_flows, :q), 
-#          x=:q, 
-#          y=:value, 
-#          color=:variable,
-#          Geom.line,
-#          Guide.XLabel("Demand"),
-#          Guide.YLabel("Flow"),
-#          Guide.colorkey("Flows"),
-#          Coord.Cartesian(ymin=0, ymax=rn.demand_range[end]),
-#          Guide.Title("Normalised edge flows") )
-# end
+"""
+Plots the normalised edge flows.
+"""
+function plot_normalised_flows(rn::RoadNetwork, regime="UE")
+    data_flows = normflows_data_frame(rn, regime)
+    #layers = [ layer( x=data_flows[1], y=data_flows[i], Geom.line, Theme(defaul_color=distinguishable_colors(size(data_flows)[2])[i])) for i in 2:length(flows) ]
+    plot(melt(data_flows, :q), 
+         x=:q, 
+         y=:value, 
+         color=:variable,
+         Geom.line,
+         Guide.XLabel("Demand"),
+         Guide.YLabel("Flow (normalised)"),
+         Guide.colorkey("Flows"),
+         Coord.Cartesian(ymin=0, ymax=1),
+         Guide.Title("Normalised edge flows") )
+end
