bf4edb13a85e00ef1a202f9d14e8e1dc6650187a
aespinosav <alonso.espinosa@bristol.ac.uk>
2016-03-14 14:15:19 +0000

    Added lattice function to make a m*n square lattice graph

--- lib_of_graphs.jl
+++ lib_of_graphs.jl
@@ -30,3 +30,18 @@ a_braess = Graph([0 1 1 1;
                 0 0 0 1;
                 0 0 0 0])
 
+"""
+Makes a square lattice graph of m * n with both incoming and outgoing 
+edges between neighbours.
+"""
+function lattice(m,n)
+    N = m*n # number of nodes
+    A = zeros(Int, N,N) # start with zero matrix
+    for k in 1:N
+        (k%n != 0) && (A[k,k+1] = 1) # If not at right edge
+        ((k-1)%n != 0) && (A[k, k-1] = 1) # If not at left edge
+        (k+n <= N) && (A[k,k+n] = 1) # If not at bottom
+        (k-n > 0) && (A[k,k-n] = 1) # If not at top
+    end
+    Graph(A)
+end
