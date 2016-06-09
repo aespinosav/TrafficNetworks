9bc010c1ad5c34fdc6b9809d90a629b0bd38cf83
aespinosav <alonso.espinosa@bristol.ac.uk>
2016-06-09 20:46:01 +0100

    I have changed the order of the included files so i get definitions of types before using them in the functions

--- TrafficNetworks.jl
+++ TrafficNetworks.jl
@@ -6,23 +6,25 @@ import Base.show
 
 export 
     Node, Edge, Graph, RoadNetwork,
+    TrafficAssignment, DataFrame,
     add_node!, add_edge!, connect!, num_nodes, num_edges,
     in_edges_idx, out_edges_idx, 
     adjacency_matrix, incidence_matrix,
     replace_OD_matrix!, make_ta_problem,
-    ta_solve, ta_solve!, p2, p3, lol, a_lol, braess,
+    ta_solve!, p2, p3, lol, a_lol, braess,
     a_braess, lattice, total_cost, edge_costs, marginal_edge_costs,
-    flows_data_frame, normflows_data_frame,
-    plot_flows, plot_normalised_flows, TrafficAssignment, DataFrame
+    flows_data_frame, normflows_data_frame, unpack_sols,
+    plot_flows, plot_normalised_flows 
 
 ## Includes
 
 include("graphs.jl")
 include("road_networks.jl")
+include("traffic_assignment.jl")
 include("ta_solver.jl")
 include("lib_of_graphs.jl")
 include("analysis_functions.jl")
 include("ta_plotting.jl")
-include("traffic_assignment.jl")
+
 
 end
