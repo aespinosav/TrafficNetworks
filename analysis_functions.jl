50ae8a31530f954f057b4ff4ee68b6643dc8c248
aespinosav <alonso.espinosa@bristol.ac.uk>
2016-06-15 15:57:05 +0100

    changed analysis functions file, this needt to be checked again

--- analysis_functions.jl
+++ analysis_functions.jl
@@ -7,35 +7,21 @@ value in the demand range.
 
 TA problem has to have been solved
 """
-function total_cost(rn::RoadNetwork; regime="UE")
-    if regime == "UE"
-        flows = rn.flows_ue
-    else
-        flows = rn.flows_so
-    end
-    
-    m = num_edges(rn)
-    num_sols = size(flows)[2]
-    num_qs = length(rn.demand_range)
-    costs = [dot(rn.a,flows[:,i]) + dot(flows[:,i],diagm(rn.b)*flows[:,i]) for i in 1:num_qs]
+function total_cost(ta::TrafficAssignment)
+    m = num_edges(ta.rn)
+    #num_sols = size(flows)[2]
+    num_qs = length(ta.demand_range)
+
+    costs = [dot(ta.rn.a, Array(ta.flows[i, 2:end])'[:]) + dot(Array(ta.flows[i, 2:end])'[:],diagm(ta.rn.b)*Array(ta.flows[i, 2:end])'[:]) for i in 1:num_qs]
 end
 
 """
 Returns the marginal cost of each edge (cost * flow) in the same format as the
 flows (rows correspond to edges, columns to demand level)
 """
-function marginal_edge_costs(rn::RoadNetwork; regime="UE")
-    m = num_edges(rn)
-
-    if regime=="UE"
-        flows = rn.flows_ue
-    else
-        flows = rn.flows_so
-    end
-
-    num_sols = size(flows)[2]
-    num_qs = length(rn.demand_range)
-    costs = (rn.a .+ rn.b .* flows)
+function marginal_edge_costs(ta::TrafficAssignment)
+    flows = Array(ta.flows[2:end])'
+    costs = (ta.rn.a .+ (ta.rn.b .* flows))
 end
 
 
@@ -43,17 +29,10 @@ end
 Returns the actual cost of each edge (cost * flow) in the same format as the
 flows (rows correspond to edges, columns to demand level)
 """
-function edge_costs(rn::RoadNetwork; regime="UE")
+function edge_costs(ta::TrafficAssignment)
     m = num_edges(rn)
     
-    if regime=="UE"
-        flows = rn.flows_ue
-    else
-        flows = rn.flows_so
-    end
-    
-    num_sols = size(flows)[2]
-    num_qs = length(rn.demand_range)
+    flows = Array(ta.flows[2:end])'
     costs = flows .* (rn.a .+ rn.b .* flows)
 end
 
