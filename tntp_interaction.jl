b9bce3e7db668cba7565e9bb732284589c190c4d
aespinosav <alonso.espinosa@bristol.ac.uk>
2017-02-02 16:48:17 +0000

    Changed the end point of the open() do block. Seems to be more robust this way

--- tntp_interaction.jl
+++ tntp_interaction.jl
@@ -14,32 +14,32 @@ function load_tntp_to_dataframe(filename)
 
     open(filename, "r") do f
         lines = readlines(f)
+
+        splits = map(x -> split(x, '\t'), lines) #separate according to separator
+        splits = remove_from_split_string(splits, "\n", "", ";\n", "~", "~ ") # get rid of unwanted characters
+        splits = splits[find(x -> length(x) != 0, splits)] # get rid of empty lines
+
+        zeroth_line = find(x -> x[1] == "<END OF METADATA>", splits)[1]
+        header_line = zeroth_line + 1
+        begining_line = zeroth_line + 2
+        end_line = length(splits)
+
+        n_edges = end_line - header_line
+
+        dat = DataFrame()
+
+        for i in 1:2
+            dat[Symbol(splits[header_line][i])] = @data [parse(Int, row[i]) for row in splits[begining_line:end] ]
+        end
+        for i in 3:length(splits[header_line])
+            dat[Symbol(splits[header_line][i])] = @data [parse(Float64, row[i]) for row in splits[begining_line:end] ]
+        end
+        return dat
     end
-
-    splits = map(x -> split(x, '\t'), lines) #separate according to separator
-    splits = remove_from_split_string(splits, "\n", "", ";\n", "~", "~ ") # get rid of unwanted characters
-    splits = splits[find(x -> length(x) != 0, splits)] # get rid of empty lines
-
-    zeroth_line = find(x -> x[1] == "<END OF METADATA>", splits)[1]
-    header_line = zeroth_line + 1
-    begining_line = zeroth_line + 2
-    end_line = length(splits)
-
-    n_edges = end_line - header_line
-
-    data = DataFrame()
-
-    for i in 1:length(splits[header_line])
-        if i == 1 || 2
-            typ = Int
-        else
-            typ = Float64
-        end 
-        data[Symbol(splits[header_line][i])] = @data [parse(typ, row[i]) for row in splits[begining_line:end] ]
-    end
-    data
 end
 
-function convert_to_tn(data_frame_from_tntp)
-    
-end
+# function convert_to_tn(data_frame_from_tntp)
+#
+       
+# end
+       
