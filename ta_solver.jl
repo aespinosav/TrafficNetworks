cd62ce99f4a5958d15d1679f158d4a85c7d4082d
aespinosav <alonso.espinosa@bristol.ac.uk>
2017-03-17 21:20:54 +0000

    Changed everything to work with sparse matrices. Better for large networks. As of this commit, the  Grap to RoadNetwork into tasolve is working as expected.

--- ta_solver.jl
+++ ta_solver.jl
@@ -1,28 +1,29 @@
 # Functions and calls to Convex.jl for solving Traffic Assignment
-
 """
 Returns the equality constraints needed for the TA optimisation
-of the RoadNetwork rn and the demand level q. The variable that is 
-used in the problem must also be passed as an argument x to the function
+of the RoadNetwork 'rn' and the demand level 'q'. The variable that is 
+used in the problem must also be passed as an argument 'x' to the function
+
+This version of the function uses sparse matrices and sparse arrays now...
 """
-function make_eq_constratints(rn::RoadNetwork, q, x::Variable)
+function make_eq_constratints(rn::RoadNetwork, OD, q, x::Variable)
     n = num_nodes(rn)
     m = num_edges(rn)
     M = incidence_matrix(rn.g)
 
-    d = zeros(n) #changed this since it was giving problems when m != n... have to tes but it looks right.
+    d = sparsevec(zeros(n)) #changed this since it was giving problems when m != n... have to test but it looks right.
     flow_counter = 1
-        for i in 1:n
-            for j in 1:n
-                if rn.OD[i,j] > 0.0
-                    d[i] = -q[flow_counter]*rn.OD[i,j]
-                    d[j] = q[flow_counter]*rn.OD[i,j]
-                    flow_counter += 1
-                end
-            end
+    
+    indices = find(OD)
+    for k in indices
+        i, j = ind2sub(OD, k)
+        if OD[i,j] > 0
+            d[i] = -q[flow_counter]*OD[i,j]
+            d[j] = q[flow_counter]*OD[i,j]
+            flow_counter += 1
         end
-
-    eq_constraints = incidence_matrix(rn.g)*x == d
+    end
+    eq_constraints = M*x == d
 end
 
 """
@@ -34,43 +35,28 @@ This should be a general method for multiple OD pairs, but it only works for 1 O
 
 This function uses Convex.jl
 """
-function make_ta_problem(rn::RoadNetwork, q, regime)
+function make_ta_problem(rn::RoadNetwork, OD, q, regime)
     
-    n = num_nodes(rn)
-    m = num_edges(rn)
     a = rn.a
     b = rn.b
+    m = num_edges(rn.g)
 
     #num_flows = sum(map(sign, rn.OD))
-
-    M = incidence_matrix(rn.g)
-
     x = Variable(m)
-    d = zeros(m)
     
-    flow_counter = 1
-    for i in 1:n
-        for j in 1:n
-            if rn.OD[i,j] > 0.0
-                d[i] = -q[flow_counter]*rn.OD[i,j]
-                d[j] = q[flow_counter]*rn.OD[i,j]
-                flow_counter += 1
-            end
-        end
-    end
-
     if regime == "UE"
-        cost_function = dot(a, x) + 0.5*quadform(x,diagm(b))
+        cost_function = dot(rn.a, x) + 0.5*quadform(x, diagm(rn.b))
     elseif regime == "SO"
-        cost_function = dot(a,x) + quadform(x,diagm(b))
+        cost_function = dot(rn.a, x) + quadform(x, diagm(rn.b))
     else
         error("Regime must be either 'UE' or 'SO'...")
     end
 
     # Constraints to be passes to Convex.jl problem
-    eq_constraints = make_eq_constratints(rn, q, x)
+    eq_constraints = make_eq_constratints(rn, OD, q, x)
     ineq_constraints = x >= 0
 
+    #Make Convex problem
     problem = minimize(cost_function, eq_constraints, ineq_constraints)
 
     return problem, x
@@ -90,9 +76,10 @@ Calls function make_ta_problem
 It returns a 2-dimensional array of the solution. The first row corresponds to the 
 first element (first edge) and so on... There is a column for every demand step.
 """
-function ta_solve(rn::RoadNetwork, q_range::Array{Float64,1}; regime="UE", logfile_name="log_ta_solve.txt")
+function ta_solve(rn::RoadNetwork, OD, q_range::Array{Float64,1}; regime="UE", logfile_name="log_ta_solve.txt")
+
     println("Will solve $regime, TA problem  for $(length(q_range)) values of demand...")
-    
+
     sols = Array{Float64}[]
 
     #redirect output of Convex solver to a log file to avoid screen clutter
@@ -100,14 +87,15 @@ function ta_solve(rn::RoadNetwork, q_range::Array{Float64,1}; regime="UE", logfi
     f = open(logfile_name, "w")
     redirect_stdout(f)
     println("Will solve $regime, TA problem  for $(length(q_range)) values of demand...\n")
-    problem, x = make_ta_problem(rn, q_range[1], regime)
-    #first solution
+
+    problem, x = make_ta_problem(rn, OD, q_range[1], regime)
+    #first solution individually to start with the warmstart later
     solve!(problem)
     push!(sols, x.value)
-    # iterates next optimisation routines with warmstart
+    #Iterates next optimisation routines with warmstart
     if length(q_range) > 1
         for q in q_range[2:end]
-            problem.constraints[1] = make_eq_constratints(rn, q, x)
+            problem.constraints[1] = make_eq_constratints(rn, OD, q, x)
             solve!(problem, warmstart=true)
             push!(sols, x.value)
         end
@@ -120,60 +108,4 @@ function ta_solve(rn::RoadNetwork, q_range::Array{Float64,1}; regime="UE", logfi
     unpack_sols(sols)
 end
 
-ta_solve(rn::RoadNetwork, q::Float64; regime="UE", logfile_name="log_ta_solve.txt") = ta_solve(rn, [q], regime=regime, logfile_name=logfile_name)
-
-
-"""
-Returns solutions to the traffic assignment problem for a given range of demands: q_range.
-Calls function make_ta_problem
-
-It returns a 2-dimensional array of the solution. The first row corresponds to the 
-first element (first edge) and so on... There is a column for every demand step.
-
-This function also updates the RoadNetwork rn to contain the demand range and 
-the flow solutions.
-
-ta_solve(rn, q_range, regime, logfile_name)
-                     |  
-                      optional from here ->
-"""
-function ta_solve!(rn::RoadNetwork, q_range::Array{Float64,1}; regime="UE", logfile_name="log_ta_solve.txt")
-    println("Will solve $regime, TA problem  for $(length(q_range)) values of demand...\n")
-    
-    rn.demand_range = q_range
-
-    sols = Array{Float64}[]
-
-    #redirect output of Convex solver to a log file to avoid screen clutter
-    originalSTDOUT = STDOUT
-    f = open(logfile_name, "w")
-    redirect_stdout(f)
-
-    problem, x = make_ta_problem(rn, q_range[1], regime)
-    #first solution
-    solve!(problem)
-    push!(sols, x.value)
-    # iterates next optimisation routines with warmstart
-    if length(q_range) > 1
-        for q in q_range[2:end] # We are omitting the first value in q_Range as it is assumed you are starting from 0. This should probably change...
-            problem.constraints[1] = make_eq_constratints(rn, q, x)
-            solve!(problem, warmstart=true)
-            push!(sols, x.value)
-        end
-    end
-
-    # return stdout to original settings (closes logfile as well)
-    close(f)
-    redirect_stdout(originalSTDOUT)
-
-    flows = unpack_sols(sols)
-    if regime=="UE"
-        rn.flows_ue = flows
-    else
-        rn.flows_so = flows
-    end
-
-    return flows
-end 
-
-ta_solve!(rn::RoadNetwork, q::Float64; regime="UE", logfile_name="log_ta_solve.txt") = ta_solve!(rn, [q], regime=regime, logfile_name=logfile_name)
+ta_solve(rn::RoadNetwork, OD, q::Float64; regime="UE", logfile_name="log_ta_solve.txt") = ta_solve(rn, [q], regime=regime, logfile_name=logfile_name)
