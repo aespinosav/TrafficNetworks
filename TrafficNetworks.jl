362274986f1b887af07a19ae9af8149da2a4430c
aespinosav <alonso.espinosa@bristol.ac.uk>
2017-06-27 12:21:43 +0100

    Changed exports

--- TrafficNetworks.jl
+++ TrafficNetworks.jl
@@ -5,13 +5,15 @@ import Base.show
 
 export 
     Node, Edge, Graph, RoadNetwork,
-    add_node!, add_edge!, connect!, num_nodes, num_edges,
-    in_edges_idx, out_edges_idx, 
+    add_node!, add_edge!, connect!, #Do these *need* to be exported?
+    num_nodes, num_edges,
+    in_edges_idx, out_edges_idx, #Do these *need* to be exported?
     adjacency_matrix, incidence_matrix,
     adjacency_matrix_non_sparse, incidence_matrix_non_sparse,
     replace_OD_matrix!, od_pairs, od_matrix_from_pair, make_ta_problem,
-    ta_solve, ta_solve!, p2, p3, lol, a_lol, braess,
-    a_braess, lattice, total_cost, edge_costs, marginal_edge_costs,
+    ta_solve, ta_solve!,
+    p2, p3, lol, a_lol, braess, a_braess, lattice, # These could be in an atlas type module by themselves
+    total_cost, edge_costs, marginal_edge_costs,
     flows_data_frame, normflows_data_frame,
     plot_flows, plot_normalised_flows
 
