a93ae7950ecf30d8ad2e89bbc955312cb7fd947f
aespinosav <alonso.espinosa@bristol.ac.uk>
2018-02-08 16:05:55 +0000

    Did a bit of cleaning on TrafficNetworks.jl

--- TrafficNetworks.jl
+++ TrafficNetworks.jl
@@ -3,17 +3,22 @@ module TrafficNetworks
 using Convex, SCS, Gadfly, DataFrames, UnicodePlots
 import Base.show, Base.length
 
-export 
+export
+#   From graphs.jl and road_networks.jl
     Node, Edge, Graph, RoadNetwork,
     add_node!, add_edge!, connect!, #Do these *need* to be exported?
     num_nodes, num_edges, node_positions,
     in_edges_idx, out_edges_idx, #Do these *need* to be exported?
     adjacency_matrix, incidence_matrix,
     adjacency_matrix_non_sparse, incidence_matrix_non_sparse,
-    replace_OD_matrix!, od_pairs, od_matrix_from_pair, make_ta_problem,
-    ta_solve, ta_solve!,
-    p2, p3, lol, a_lol, braess, a_braess, lattice, # These could be in an atlas type module by themselves
-    total_cost, edge_costs, marginal_edge_costs,
+    replace_OD_matrix!, od_pairs, od_matrix_from_pair, 
+#   From ta_solver.jl    
+    make_ta_problem, ta_solve, ta_solve!,
+#   From lib_of_graphs.jl
+    p2, p3, lol, a_lol, braess, a_braess, lattice, 
+#   From analysis_functions.jl
+    total_cost, costs,
+#   From ta_plotting.jl
     flows_data_frame, normflows_data_frame,
     plot_flows, plot_normalised_flows
 
